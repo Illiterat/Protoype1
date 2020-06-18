94c77109c2f36374c29333fc8fc2fc638d43dc71
NoLivesRemaining <31869017+NoLivesRemaining@users.noreply.github.com>
2020-06-18 20:41:40 +0100

    Attack phase done

--- Protoype/Assets/Scripts/CoreMovement.cs
+++ Protoype/Assets/Scripts/CoreMovement.cs
@@ -6,9 +6,14 @@ public class CoreMovement : MonoBehaviour
 {
     public int spacesY = 3; //Size of array Columns
     public int spacesX = 3; //Size of array Rows
-    public GameObject player;
+
+    public DamageAndHealthValues valueScipt; // accessing the DamageAndHealthValues script
+    public TileTriggerScript currentSpace;
+
     public List<GameObject> spacesInput = new List<GameObject>(); //For taking the objects in from the inspector
-    public GameObject[,] spaces = new GameObject[3,3]; //The array of possitions
+    public GameObject[,] spaces = new GameObject[3, 3]; //The array of possitions
+    public GameObject player;
+
     int currentX; //Current X position in array.
     int currentY; //Current Y position in array.
 
@@ -30,63 +35,78 @@ public class CoreMovement : MonoBehaviour
 
             //Loop through the list and assign elements to the correct position in array.
             //i controls column itteration, j controls row itteration.
-            for(int i = 0; i < spacesY; i++)
+            for (int i = 0; i < spacesY; i++)
             {
-                for(int j = 0; j < spacesX; j++)
+                for (int j = 0; j < spacesX; j++)
                 {
                     //Debug.Log(i + " " + j); //Used for debuging array asigning.
-                    spaces[j,i] = spacesInput[listElement];
+                    spaces[j, i] = spacesInput[listElement];
                     listElement++; //Move on to the next element of the list.
                 }
             }
-            
+
             //Set the starting point for the player. (at the moment coordinate 1,1)
             currentX = 1;
             currentY = 1;
-            player.transform.position = spaces[currentX,currentY].transform.position;
+            player.transform.position = spaces[currentX, currentY].transform.position;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckForActivation();
+
         if (Input.GetKeyDown(KeyCode.D)) //Right movement
         {
             //Check if space to move to exists
-            if (currentX+1 < spacesX)
+            if (currentX + 1 < spacesX)
             {
                 currentX++; //Update variable representing it's position
                 //Move the player
-                player.transform.position = spaces[currentX,currentY].transform.position;
+                player.transform.position = spaces[currentX, currentY].transform.position;
                 //Debug.Log(currentX + "," + currentY); //Uncomment to display position as you move
             }
         }
         else if (Input.GetKeyDown(KeyCode.A)) //Left movement
         {
-            if (currentX-1 >= 0)
+            if (currentX - 1 >= 0)
             {
                 currentX--;
-                player.transform.position = spaces[currentX,currentY].transform.position;
+                player.transform.position = spaces[currentX, currentY].transform.position;
                 //Debug.Log(currentX + "," + currentY); //Uncomment to display position as you move
             }
         }
         else if (Input.GetKeyDown(KeyCode.W)) //Up movement
         {
-            if (currentY-1 >= 0)
+            if (currentY - 1 >= 0)
             {
                 currentY--;
-                player.transform.position = spaces[currentX,currentY].transform.position;
+                player.transform.position = spaces[currentX, currentY].transform.position;
                 //Debug.Log(currentX + "," + currentY); //Uncomment to display position as you move
             }
         }
         else if (Input.GetKeyDown(KeyCode.S)) //Down movement
         {
-            if (currentY+1 < spacesY)
+            if (currentY + 1 < spacesY)
             {
                 currentY++;
-                player.transform.position = spaces[currentX,currentY].transform.position;
+                player.transform.position = spaces[currentX, currentY].transform.position;
                 //Debug.Log(currentX + "," + currentY); //Uncomment to display position as you move
             }
         }
     }
+
+    void CheckForActivation()
+    {
+       currentSpace = spaces[currentX, currentY].GetComponent<TileTriggerScript>();
+    
+       if(currentSpace.activated == true)
+        {
+            valueScipt.playerDamage++; // increasing the players damage
+            currentSpace.render.sharedMaterial = currentSpace.color[0];
+            currentSpace.activated = false;
+        }
+    }
+
 }
