8eadb84b3ec8e57c38081235ff221586028ca73b
Satan's Cat <endamkeane@gmail.com>
2020-07-26 20:52:22 +0100

    Starting ground up attack functionality

---
+++ Protoype/Assets/Scripts/Attacks/OrangeBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeBall : MonoBehaviour, AttackInterface
+{
+    public float firespeed {get; set;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*private void Launch(GameObject emitter)
+    {
+        GameObject projectileClone = Instantiate(projectile, emitter.transform.position, emitter.transform.rotation);
+        projectileClone.GetComponent<Rigidbody2D>().velocity = -transform.right * fireSpeed;
+    }*/
+
+    public void Use()
+    {
+
+    }
+}
---
+++ Protoype/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Interface describing a function for activating a pattern.
+public interface PatternInterface
+{
+    float firespeed{ get; set;}
+    void Begin();
+}
+
+
+public interface AttackInterface
+{
+    float firespeed {get; set;}
+    void Use();
+}
+
