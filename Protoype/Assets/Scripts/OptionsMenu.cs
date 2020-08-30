c7fe1113a69f58949d50fd6db11f2a45f2dd8c33
Satan's Cat <endamkeane@gmail.com>
2020-08-30 14:20:30 +0100

    Fixing merge bits

--- Protoype/Assets/Scripts/OptionsMenu.cs
+++ Protoype/Assets/Scripts/OptionsMenu.cs
@@ -2,44 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
-using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public AudioConfiguration Config;
 
-    public TMP_Dropdown resolutionDropdown;
-   // public TextMeshProUGUI resolutionDropdown;
-
-    Resolution[] resolutions;
-
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-
-        for(int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void SetMasterVolume (float MasterVolume)
+   public void SetMasterVolume (float MasterVolume)
     {
         audioMixer.SetFloat("MasterVolume", MasterVolume);
     }
@@ -52,34 +20,5 @@ public class OptionsMenu : MonoBehaviour
     public void SetEffectsVolume(float EffectsVolume)
     {
         audioMixer.SetFloat("EffectsVolume", EffectsVolume);
-    }
-
-    public void SetMono(bool Mono)
-    {
-        if(Mono == true)
-        {
-            Config.speakerMode = AudioSpeakerMode.Mono;
-        }
-
-        if (Mono == false)
-        {
-            Config.speakerMode = AudioSpeakerMode.Stereo;
-        }
-    }
-
-    public void SetQuality(int QualityIndex)
-    {
-        QualitySettings.SetQualityLevel(QualityIndex);
-    }
-
-    public void SetFullScreen(bool isFullScreen)
-    {
-        Screen.fullScreen = isFullScreen;
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
