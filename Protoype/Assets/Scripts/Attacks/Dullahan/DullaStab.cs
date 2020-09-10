3566cc7b09f69d4757535f9347639d375b091694
Satan's Cat <endamkeane@gmail.com>
2020-09-10 21:47:07 +0100

    Started work on the second dullahan attack

---
+++ Protoype/Assets/Scripts/Attacks/Dullahan/DullaStab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DullaStab : MonoBehaviour
+{
+    public void Launch(GameObject[] tiles)
+    {
+        foreach (GameObject t in tiles)
+        {
+            
+        }
+    }
+}
