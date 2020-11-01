6e9fcf257a37dfc1a64de47ee105bc66f6c266d5
Satan's Cat <endamkeane@gmail.com>
2020-11-01 18:30:49 +0000

    Adding patterns

---
+++ Protoype/Assets/Scripts/Patterns/Dullahan/D_2_1.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_2_1 : PatternSuper
+{
+    // Instances of the attacks necisary for this pattern
+    public Dullahead headScript;
+    public GameObject head;
+    
+    public DullaStab stabScript;
+    public DullaWhip whipScript;
+    //public DullaReverseWhip reverseWhipScript;
+    public GameObject hazard;
+
+    private GameObject[] tileRowTop;
+    private GameObject[] tileRowMid;
+    private GameObject[] tileRowBot;
+    private GameObject[] tileColMid;
+    private GameObject[] tileColR;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        phase = 2; //Set the phase for this attack
+        
+        //Set up for the head throw.
+        headScript = gameObject.AddComponent<Dullahead>();
+        headScript.projectile = this.head;
+
+        //Set up for the spine stab
+        /*if(gameObject.GetComponent(typeof(DullaStab)) == null)
+        {*/
+            stabScript = gameObject.AddComponent<DullaStab>();
+            stabScript.hazard = this.hazard;
+        /*}
+        else
+        {
+            stabScript = gameObject.GetComponent(typeof(DullaStab)) as DullaStab;
+        }*/
+
+        //Set up for spine whip
+        /*if(gameObject.GetComponent(typeof(DullaWhip)) == null)
+        {*/
+            whipScript = gameObject.AddComponent<DullaWhip>();
+            whipScript.hazard = this.hazard;
+        /*}
+        else 
+        {
+            whipScript = gameObject.GetComponent(typeof(DullaWhip)) as DullaWhip;
+        }*/
+
+
+        //Set the rows and columns
+        tileRowTop = new GameObject[] {tiles[0],tiles[1],tiles[2]};
+        tileRowMid = new GameObject[] {tiles[3],tiles[4],tiles[5]};
+        tileRowBot = new GameObject[] {tiles[6],tiles[7],tiles[8]};
+        tileColMid = new GameObject[] {tiles[1],tiles[4],tiles[7]};
+        tileColR = new GameObject[] {tiles[2],tiles[5],tiles[8]};
+    }
+
+
+    //The word "override" here lets me re-write what the function does
+    public override IEnumerator Begin(GameObject emitter)
+    {
+        headScript.fireSpeed = fireSpeed;
+        //Down mid, across mid, big wait, bottom line, top line right line
+        yield return new WaitForSeconds(waitTime);
+        headScript.Launch(emitters[0]);
+        //Debug.Log(tileColMid);
+        whipScript.Launch(tileColMid);
+        yield return new WaitForSeconds(waitTime);
+        whipScript.Launch(tileColR);
+        yield return new WaitForSeconds(waitTime*2);
+        headScript.Launch(emitters[1]);
+        yield return new WaitForSeconds(waitTime);
+        stabScript.Launch(tileRowTop);
+        yield return new WaitForSeconds(waitTime);
+        stabScript.Launch(tileRowMid);
+        yield return new WaitForSeconds(waitTime);
+        stabScript.Launch(tileRowBot);
+        
+    }
+}
